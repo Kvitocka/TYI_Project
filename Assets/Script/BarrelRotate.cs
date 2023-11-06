98959dc6d0406aa54670eb612d6c0b3988e36711
Kvitocka <138381513+Kvitocka@users.noreply.github.com>
2023-11-06 10:43:34 +0200

    AddBarrelRotation

--- Assets/Script/BarrelRotate.cs
+++ Assets/Script/BarrelRotate.cs
@@ -1,13 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BarrelRotate : MonoBehaviour
 {
     // Start is called before the first frame update
-    void RotateBarrelTo()
-    {
-        
-    }
+    [SerializeField] private Transform barrel;
+    [SerializeField] private float barrelSpeed;
+    [SerializeField] private GameObject TargetObject;
+
+	Quaternion barrelRotation(Vector3 Target)
+	{
+		Target.z = Camera.main.transform.position.z;
+		Vector3 direction = Camera.main.ScreenToWorldPoint(Target) - transform.position;
+		float angle  = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		return Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+	
+
+	void Update()
+	{
+		barrel.rotation = Quaternion.Lerp(barrel.rotation, barrelRotation(TargetObject.transform.position), barrelSpeed * Time.deltaTime);
+	}
 
 }
