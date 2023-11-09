3c5f79ef1d1d024b2bf72df8ee9e16ce7a390226
TechPriest2187 <nazkarukr2007@gmail.com>
2023-11-09 10:01:42 +0200

    AF_update
    Target's linear accelerated and non-accelerated movement

---
+++ Assets/Script/AF_scripts/linear_movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linear_movement : MonoBehaviour
+{
+    public Vector3 target_direction = new Vector3(1.0f, 0.0f, 0.0f);
+    public float target_speed = 5.0f;
+    public float target_acceleration = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(target_direction * target_speed * target_acceleration * Time.deltaTime);
+    }
+}
--- Assets/Script/AF_scripts/target_spawn.cs
+++ Assets/Script/AF_scripts/target_spawn.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class target_spawn : MonoBehaviour
 {
- public void Update()
- {
-    transform.Translate(Vector3.forward * 10 * Time.deltaTime);
- }
-    
+    public GameObject targetPrefab; // Prefab of the flying target object
+    public float movementSpeed = 5f; // Speed of movement along the X-axis
+    private GameObject targetObject; // Reference to the spawned target object
+
+    void Start()
+    {
+        // Spawn the flying target object at the specified position
+        SpawnTargetObject(new Vector3(10f, 10f, 10f));
+    }
+
+    void Update()
+    {
+    }
+
+    // Spawn the target object at the specified position
+    void SpawnTargetObject(Vector3 spawnPosition)
+    {
+        if (targetPrefab != null)
+        {
+            targetObject = Instantiate(targetPrefab, spawnPosition, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogError("Target prefab is not assigned!");
+        }
+    }
 }
