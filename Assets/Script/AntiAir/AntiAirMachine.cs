e56690366d3016e562c238f5f792078ceca15094
Bohdan Marchak <50302855+marchakboh@users.noreply.github.com>
2023-11-09 13:07:16 +0200

    Added possible AntiAirSystem machine

---
+++ Assets/Script/AntiAir/AntiAirMachine.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class AntiAirMachine : MonoBehaviour
+{
+    [SerializeField] private String trackedObjectsTag;
+
+    private TurretComponent turretComponent;
+
+    private List<GameObject> collideObjects;
+    private bool needToTrack;
+
+    private void Awake()
+    {
+        turretComponent = GetComponentInChildren<TurretComponent>();
+        if (turretComponent == null)
+        {
+            Debug.LogError("TurrentComponent not found in childrens.");
+        }
+    }
+
+    void Start()
+    {
+        collideObjects = new List<GameObject>();
+        needToTrack = false;
+    }
+
+    void Update()
+    {
+        if (needToTrack && collideObjects.Count > 0)
+        {
+            GameObject collideObject = collideObjects[0];
+            turretComponent.TargetTo(collideObject.transform.position);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(trackedObjectsTag))
+        {
+            needToTrack = true;
+            collideObjects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(trackedObjectsTag))
+        {
+            collideObjects.Remove(other.gameObject);
+            
+            if (collideObjects.Count == 0)
+            { 
+                needToTrack = false;
+            }
+        }
+    }
+}
---
+++ Assets/Script/AntiAir/TurretComponent.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretComponent : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 45.0f;
+
+    private CannonComponent cannonComponent;
+
+    private bool needRotate;
+    private Vector3 targetPosition;
+
+    private void Awake()
+    {
+        cannonComponent = GetComponentInChildren<CannonComponent>();
+        if (cannonComponent == null)
+        {
+            Debug.LogError("CannonComponent not found in childrens.");
+        }
+    }
+
+    void Start()
+    {
+        needRotate = false;
+    }
+
+    void Update()
+    {
+        if (needRotate)
+        {
+            Vector3 direction = targetPosition - transform.position;
+            float angleInRad = Mathf.Atan2(direction.z, direction.x);
+            float angleInDegr = angleInRad * Mathf.Rad2Deg * -1.0f;
+
+            float angleDiff = Mathf.DeltaAngle(transform.eulerAngles.y, angleInDegr);
+
+            if (Mathf.Abs(angleDiff) > 0f)
+            {
+                float rotationDirection = Mathf.Sign(angleDiff);
+
+                float rotationAmount = rotationSpeed * Time.deltaTime * rotationDirection;
+
+                transform.Rotate(0f, rotationAmount, 0f);
+            }
+        }
+    }
+
+    public void TargetTo(Vector3 coordinate)
+    {
+        targetPosition = coordinate;
+        needRotate = true;
+        cannonComponent.TargetTo(coordinate);
+    }
+}
