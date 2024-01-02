11107805aff79a05650a1917e5b74df89c7ce718
TechPriest2187 <nazkarukr2007@gmail.com>
2024-01-02 17:00:14 +0200

    Explosion

--- Assets/Object/SceneObjeckt/AirForce/Target/Script/BulletReaction.cs
+++ Assets/Object/SceneObjeckt/AirForce/Target/Script/BulletReaction.cs
@@ -5,11 +5,12 @@ using System;
 
 public class BulletReaction : MonoBehaviour
 {
-    public Collider collider;
+    public GameObject explosionEffect;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("bullet"))
         {
+            Instantiate(explosionEffect, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
