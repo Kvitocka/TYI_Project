46ed3b029bedf0ae9946d4ef33f626e74a592d77
Kvitocka <138381513+Kvitocka@users.noreply.github.com>
2024-01-03 02:22:21 +0200

    FinalyComit(no)

--- Assets/Object/SceneObjeckt/AirForce/Target/Script/linear_movement.cs
+++ Assets/Object/SceneObjeckt/AirForce/Target/Script/linear_movement.cs
@@ -1,9 +1,4 @@
-using System.Xml.Schema;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class linear_movement : MonoBehaviour
