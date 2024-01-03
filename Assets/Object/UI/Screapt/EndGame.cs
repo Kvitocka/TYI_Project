acfb20278067e54440bb88fc448d9edb0bfea380
Kvitocka <138381513+Kvitocka@users.noreply.github.com>
2024-01-03 20:38:06 +0200

    FixBag

---
+++ Assets/Object/UI/Screapt/EndGame.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void end()
+    {
+        Application.Quit();
+    }
+}
